84f6b9718b3c6168ffe5abd0895034b2260794a9
Luneux <yanselpedraza@gmail.com>
2025-07-23 17:35:46 -0500

    I FUCKING DID IT
    LETS GOOOOO

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,96 +4,53 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Filesystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import swervelib.SwerveDrive;
+import swervelib.parser.SwerveControllerConfiguration;
 import swervelib.parser.SwerveDriveConfiguration;
+
+import java.io.File;
 
 public class Robot extends TimedRobot {
 
   // Controllers
-  public final XboxController driverController = new XboxController(0);
+  private final XboxController driverController = new XboxController(0);
   private final XboxController operatorController = new XboxController(1);
-   private static final Pose2d INITIAL_POSE = new Pose2d(0, 0, new Rotation2d(0)); // Starting at origin (0,0), heading = 0 degrees.\
-  
-  // Swerve Drive Subsystem
-  SwerveDriveConfiguration swerveDriveConfiguration = new SwerveDriveConfiguration(null, null, null, isAutonomous(), null, null);
+
+  // Starting pose
+  private static final Pose2d INITIAL_POSE = new Pose2d(0, 0, new Rotation2d(0));
+
+  // Swerve Subsystem
   private SwerveDriveSubsystem swerve;
 
   @Override
-  public void robotInit() {
-  }
-    // Initialize the SwerveDriveSubsystem with the appropriate swerveDrive object
-    SwerveDrive swerveDrive = new SwerveDrive(swerveDriveConfiguration, null, SwerveDriveSubsystem.MAX_LINEAR_SPEED, INITIAL_POSE);
-    swerve = new SwerveDriveSubsystem(swerveDrive);
-  }
-
-  // Called every 20 ms, runs the Scheduler (which schedules and runs commands)
-  @Override
   public void robotPeriodic() {
-    // Optionally, put debug information on the SmartDashboard
-    swerve.periodic();
+    swerve.periodic(); // update SmartDashboard, pose, etc.
+
+    
   }
 
   @Override
-  public void disabledInit() {
-    // Logic for when the robot is disabled
+  public void robotInit() {
+    File configFolder = new File(Filesystem.getDeployDirectory(), "swerve");
+    try {
+      SwerveDrive swerveDrive = new SwerveDrive(configFolder);
+      swerve = new SwerveDriveSubsystem(swerveDrive);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
-
-  @Override
-  public void disabledPeriodic() {
-    // Any periodic logic during disabled mode (diagnostics, etc.)
-  }
-
-  @Override
-  public void autonomousInit() {
-    // Schedule autonomous commands if you have any
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-    // Autonomous periodic logic
-  }
-
-  @Override
-  public void teleopInit() {
-    // Logic for when teleop starts
-  }
+  
 
   @Override
   public void teleopPeriodic() {
-    // Get joystick inputs
-    double translationX = driverController.getLeftX();  // X-axis for translation
-    double translationY = driverController.getLeftY();  // Y-axis for translation
-    double headingX = driverController.getRightX();     // X-axis for rotation
-    double headingY = driverController.getRightY();     // Y-axis for rotation (if used)
-
-    // Apply joystick inputs to the swerve drive
+    // Drive the robot using joystick input
     swerve.driveCommand(
-      () -> translationX, // Translation in X direction (DoubleSupplier)
-      () -> translationY, // Translation in Y direction (DoubleSupplier)
-      () -> headingX,     // Rotation speed based on X axis
-      () -> headingY      // (Optionally) rotation speed based on Y axis (if used)
-  ).schedule();  // Schedules the command to run
-}
-}
-
-  @Override
-  public void testInit() {
-    // Cancel all running commands at the start of test mode
-  }
-
-  @Override
-  public void testPeriodic() {
-    // Periodic logic for test mode
-  }
-
-  @Override
-  public void simulationInit() {
-    // Initialize simulation (if necessary)
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // Periodic simulation logic (if necessary)
+        () -> driverController.getLeftX(),
+        () -> driverController.getLeftY(),
+        () -> driverController.getRightX(),
+        () -> driverController.getRightY()
+    ).schedule();
   }
 }
