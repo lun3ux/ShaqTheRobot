0b81ae9c470008f2b430f4bd6fee9a97e04c8872
Luneux <yanselpedraza@gmail.com>
2025-07-23 11:40:00 -0500

    im gonna kms myself
    YIPPIER

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,8 +22,6 @@ public class Robot extends TimedRobot {
   public SwerveDriveSubsystem swerve;
   private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -31,7 +29,6 @@ public class Robot extends TimedRobot {
   public Robot() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
   }
 
   /**
@@ -60,8 +57,6 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
