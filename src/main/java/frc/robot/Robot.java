451b32e6f5d6157227ad1514e36bfb3e837fadeb
Luneux <yanselpedraza@gmail.com>
2025-07-25 08:10:55 -0500

    Bug Fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Filesystem;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 import java.io.File;
 
@@ -18,16 +21,26 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-        try {
-            File swerveFolder = new File(Filesystem.getDeployDirectory(), "swerve");
-            SwerveDrive swerveDrive = new SwerveParser(swerveFolder).createSwerveDrive(4);
-            swerveDriveSubsystem = new SwerveDriveSubsystem(swerveDrive);
-        } catch (Exception e) {
-            e.printStackTrace();
+        boolean connected = false;
+        
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+        if (Robot.isSimulation()) {
+
         }
-        driverController = new XboxController(0);
-        swerveDriveSubsystem.zeroGyro();
-    }
+        else {
+            try {
+                File swerveFolder = new File(Filesystem.getDeployDirectory(), "swerve");
+                SwerveDrive swerveDrive = new SwerveParser(swerveFolder).createSwerveDrive(4);
+                swerveDriveSubsystem = new SwerveDriveSubsystem(swerveDrive);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            driverController = new XboxController(0);
+            swerveDriveSubsystem.zeroGyro();
+            CommandScheduler.getInstance().registerSubsystem(swerveDriveSubsystem);
+        }
+        }
 
     @Override
     public void robotPeriodic() {
